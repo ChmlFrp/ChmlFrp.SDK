a0bab0cbbc7523330c4636ba250592ea3b4b5836
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2025-05-18 16:49:02 +0800

    0.0.0.5

--- MainClass.cs
+++ MainClass.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections.Generic;
+﻿#if NET48
+using Newtonsoft.Json.Linq;
+#else
+using System.Text.Json.Nodes;
+#endif
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Win32;
-#if NET48
-using Newtonsoft.Json.Linq;
-
-#else
-using System.Text.Json;
-using System.Text.Json.Nodes;
-#endif
 
 namespace ChmlFrp.Api;
 
-public static class User
+public abstract class User
 {
     private static readonly RegistryKey Key =
         Registry.CurrentUser.CreateSubKey(@"SOFTWARE\\ChmlFrp", true);
@@ -61,7 +59,7 @@ public abstract class Api
 #if NET48
             return JObject.Parse(content);
 #else
-        return JsonNode.Parse(content);
+            return JsonNode.Parse(content);
 #endif
         }
         catch
@@ -70,16 +68,18 @@ public abstract class Api
         }
     }
 
-    public abstract class Login
+    public static class Sign
     {
-        public static bool IsLogin;
+        public static bool IsSignin;
 
-        public static async Task InitializeAsync()
+        static Sign()
         {
-            await Loginin();
+#pragma warning disable CS4014
+            Signin();
+#pragma warning restore CS4014
         }
 
-        public static async Task<string> Loginin(string name = "", string password = "")
+        public static async Task<string> Signin(string name = "", string password = "")
         {
             if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(password))
             {
@@ -101,25 +101,80 @@ public abstract class Api
             var msg = ((JObject)jObject)["msg"]?.ToString();
             var usertoken = ((JObject)jObject)["data"]?["usertoken"]?.ToString();
 #else
-var msg = ((JsonNode)jObject)?["msg"]?.ToString();
-var usertoken = ((JsonNode)jObject)?["data"]?["usertoken"]?.ToString();
+            var msg = ((JsonNode)jObject)?["msg"]?.ToString();
+            var usertoken = ((JsonNode)jObject)?["data"]?["usertoken"]?.ToString();
 #endif
             if (msg != "登录成功") return msg;
 
             User.Save(name, password, usertoken);
-            IsLogin = true;
-            await UserInformation.Load();
+            IsSignin = true;
+            await UserInfo.Load();
             return "登录成功";
         }
 
-        public static void Loginout()
+        public static void Signout()
         {
             User.Save("", "");
-            IsLogin = false;
+            IsSignin = false;
         }
     }
 
-    public abstract class UserInformation
+    public abstract class Tunnel
+    {
+        public static async Task<string> GetTunnelIniData(string name)
+        {
+            if (!Sign.IsSignin) return null;
+
+            var parameters = new Dictionary<string, string> { { "token", $"{User.Usertoken}" } };
+            var jObject = await GetApi("https://cf-v2.uapis.cn/tunnel", parameters);
+
+            if (jObject == null) return null;
+
+#if NET48
+            var data = ((JObject)jObject)["data"];
+            var msg = ((JObject)jObject)["msg"];
+
+            if (data == null) return null;
+            if (msg?.ToString() != "获取隧道数据成功") return null;
+            var node = (from tunnel in data
+                where tunnel["name"]?.ToString() == name
+                select tunnel["node"]?.ToString()).FirstOrDefault();
+#else
+            var data = ((JsonNode)jObject)["data"];
+            var msg = ((JsonNode)jObject)["msg"];
+
+            if (data == null) return null;
+            if (msg?.ToString() != "获取隧道数据成功") return null;
+
+            var node = (from tunnel in data.AsArray()
+                where tunnel!["name"]?.ToString() == name
+                select tunnel["node"]?.ToString()).FirstOrDefault();
+#endif
+
+            if (node == null) return null;
+
+            parameters = new Dictionary<string, string>
+            {
+                { "token", $"{User.Usertoken}" },
+                { "node", $"{node}" },
+                { "tunnel_names", $"{name}" }
+            };
+            jObject = await GetApi("https://cf-v2.uapis.cn/tunnel_config", parameters);
+
+#if NET48
+            data = ((JObject)jObject)["data"];
+            msg = ((JObject)jObject)["msg"];
+#else
+            data = ((JsonNode)jObject)?["data"];
+            msg = ((JsonNode)jObject)?["msg"];
+#endif
+            if (msg?.ToString() != "配置文件获取成功") return null;
+
+            return (string)data;
+        }
+    }
+
+    public abstract class UserInfo
     {
         public static string usergroup;
         public static string term;
@@ -133,14 +188,9 @@ var usertoken = ((JsonNode)jObject)?["data"]?["usertoken"]?.ToString();
         public static string abroadBandwidth;
         public static string realname;
 
-        static UserInformation()
-        {
-            Load();
-        }
-
         public static async Task Load()
         {
-            if (Login.IsLogin == false) return;
+            if (Sign.IsSignin == false) return;
 
             var parameters = new Dictionary<string, string> { { "token", $"{User.Usertoken}" } };
             var jObject = await GetApi("https://cf-v2.uapis.cn/userinfo", parameters);
@@ -160,19 +210,35 @@ var usertoken = ((JsonNode)jObject)?["data"]?["usertoken"]?.ToString();
             abroadBandwidth = data?["abroadBandwidth"]?.ToString();
             realname = data?["realname"]?.ToString();
 #else
-    var data = ((JsonNode)jObject)?["data"];
-    usergroup = data?["usergroup"]?.ToString();
-    term = data?["term"]?.ToString();
-    qq = data?["qq"]?.ToString();
-    email = data?["email"]?.ToString();
-    tunnel = data?["tunnel"]?.ToString();
-    tunnelstate = data?["tunnelstate"]?.ToString();
-    userimg = data?["userimg"]?.ToString();
-    username = data?["username"]?.ToString();
-    bandwidth = data?["bandwidth"]?.ToString();
-    abroadBandwidth = data?["abroadBandwidth"]?.ToString();
-    realname = data?["realname"]?.ToString();
+            var data = ((JsonNode)jObject)["data"];
+            usergroup = data?["usergroup"]?.ToString();
+            term = data?["term"]?.ToString();
+            qq = data?["qq"]?.ToString();
+            email = data?["email"]?.ToString();
+            tunnel = data?["tunnel"]?.ToString();
+            tunnelstate = data?["tunnelstate"]?.ToString();
+            userimg = data?["userimg"]?.ToString();
+            username = data?["username"]?.ToString();
+            bandwidth = data?["bandwidth"]?.ToString();
+            abroadBandwidth = data?["abroadBandwidth"]?.ToString();
+            realname = data?["realname"]?.ToString();
 #endif
+        }
+
+
+        public static string GetUserInfo()
+        {
+            return $"用户名：{username}\n" +
+                   $"用户组：{usergroup}\n" +
+                   $"到期时间：{term}\n" +
+                   $"QQ：{qq}\n" +
+                   $"邮箱：{email}\n" +
+                   $"隧道数：{tunnel}\n" +
+                   $"隧道状态：{tunnelstate}\n" +
+                   $"头像：{userimg}\n" +
+                   $"带宽：{bandwidth}\n" +
+                   $"境外带宽：{abroadBandwidth}\n" +
+                   $"实名信息：{realname}";
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # ChmlFrp.Api
 
-一个 ChmlFrp的.NET NUGET包。
+一个用于获得ChmlFrp API的.NET Nuget包。
+
+## 安装
+
+```bash
+dotnet add package ChmlFrp.Api
+```
+
+如果是.Net9.0直接可以安装
+如果是.Net48需要安装Newtonsoft.Json。
+
+## 使用
+
+### 登录
+
+```csharp
+using ChmlFrp.Api;
+
+// 如何想让TextBox或PasswordBox获取之前输入的错误的用户名和密码
+YourTextBox.Text = User.Username; // 密码同理
+
+// 输入用户名密码登录，返回的是登录状态的提示。
+string msg = await Api.Sign.Signin(【你的用户名】,【你的密码】);
+
+// 这才是登录状态。
+if (Api.Sign.IsSignin) Console.WriteLine("登录成功");
+else Console.WriteLine("登录失败");
+```
+
+### 登出
+
+```csharp
+using ChmlFrp.Api;
+
+Api.Sign.Signout();
+// 登出后登录状态会变成false
+// 注：登出后用户信息没有变成null。
+```
+
+### 用户信息
+
+```csharp
+using ChmlFrp.Api;
+
+//这是完整信息显示。
+string UserInfo = Api.UserInfo.GetUserInfo;
+if (UserInfo == null) return;
+Console.WriteLine(UserInfo);
+//这是单个的以email为例
+string email = Api.UserInfo.email;
+if (email == null) return;
+Console.WriteLine(email);
+// 因为用户信息是登录后才能获取的，所以需要先登录，不然会返回null。
+```
+
+### 获得隧道配置文件
+
+```csharp
+using ChmlFrp.Api;
+
+// 需要登录后才能获取隧道配置文件
+string iniData =  Api.Tunnel.GetTunnelIniData(【你的隧道名】);
+if (iniData == null) return;
+File.WriteAllText(【隧道配置文件路径】, iniData);
+```
